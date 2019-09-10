6e20c9f9874e46757942278ec8d1f5be55af11ed
Alexei Ledenev <alexei.led@gmail.com>
2019-09-10 16:30:04 +0300

    change assume role duration to 2h

--- sts-entrypoint.sh
+++ sts-entrypoint.sh
@@ -3,13 +3,13 @@ set -e
 
 # workaround SSM Agent issue https://github.com/aws/amazon-ssm-agent/issues/211 (vendoring AWS Go SDK without OIDC support)
 # assume role with web identity using AWS CLI
-echo "Assuming role with AWS STS (duration=12h) ..."
+echo "Assuming role with AWS STS (duration=2h) ..."
 
 aws sts assume-role-with-web-identity \
  --role-arn $AWS_ROLE_ARN \
  --role-session-name mh9test \
  --web-identity-token file://$AWS_WEB_IDENTITY_TOKEN_FILE \
- --duration-seconds 43200 > /tmp/cred.txt
+ --duration-seconds 7200 > /tmp/cred.txt
 
 export AWS_ACCESS_KEY_ID="$(cat /tmp/cred.txt | jq -r ".Credentials.AccessKeyId")"
 export AWS_SECRET_ACCESS_KEY="$(cat /tmp/cred.txt | jq -r ".Credentials.SecretAccessKey")"
